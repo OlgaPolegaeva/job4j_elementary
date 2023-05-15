fa024b65348ca3ff102ab306abe41f4319a3e3aa
onik <parus99@inbox.ru>
2023-05-16 01:30:57 +0300

    3.2. Загрузка файлов на github [#243562 #415052]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String [] args) {
+        System.out.println ("Olga Polegaeva");
+        System.out.println ("03.05.2023");
+    }
+}

5c7c222345963ff1075eb0ef826649b0acbe61e2
onik <parus99@inbox.ru>
2023-05-16 20:09:28 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String [] args) {
-        System.out.println ("Olga Polegaeva");
-        System.out.println ("03.05.2023");
+    public static void main(String[] args) {
+        System.out.println("Olga Polegaeva");
+        System.out.println("03.05.2023");
     }
 }

0f742d8fd38ca872871ed714df45cfc333845705
onik <parus99@inbox.ru>
2023-05-22 17:55:27 +0300

    4.1. Аргументы [#295521 #417809]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -1,18 +1,18 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    public static void hello(String name) {
-        System.out.println("Hello, " + name + ", age = " + 6 + ".");
-        System.out.println("Hello, " + name + ", age = " + 6 + ".");
-        System.out.println("Hello, " + name + ", age = " + 6 + ".");
-        System.out.println("Hello, " + name + ", age = " + 6 + ".");
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
     }
 
     public static void main(String[] arg) {
 
         String name = "Job4j";
-        char age = '6';
-        ArgMethod.hello(name);
+       int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 
 }

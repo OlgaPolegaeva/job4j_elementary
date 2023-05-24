cdd9b9e55808d72135a3eb9130bd3f2e2ef9ea50
onik <parus99@inbox.ru>
2023-05-24 20:52:44 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905 #418301]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -3,10 +3,8 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] arg) {
         String idea = "I like Java!";
-        System.out.println(idea);
         idea = idea + "But I am a newbie.";
-        int year = 2023;
-        idea = idea + year;
+        idea += "2023";
         System.out.println(idea);
 
     }

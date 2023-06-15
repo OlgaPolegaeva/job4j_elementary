78f9edbf915b295e0bfd82c109c401e860927af3
onik <parus99@inbox.ru>
2023-06-15 22:43:29 +0300

    5.4. Протеиновая диета (цикл while) [#156313 #423215]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -7,8 +7,6 @@ public class Fitness {
             ivan *= 3;
             nik *= 2;
             month++;
-            if (ivan > nik)
-                break;
         }
         return month;
     }
